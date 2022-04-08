9fd4ab993e4cc07c39bf84f41fed87fc194f124a
itsmmagain <mmtsalman009@gmail.com>
2022-04-07 21:12:29 -0700

    100-intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return always 0 (success)
+ */
+void main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar,");
+	fprintf(stderr, "2015-10-19\n");
+	return (0);
+}
+

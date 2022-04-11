e74bbe910a8b733eee37b8bc7d3dda11c8f77491
itsmmagain <mmtsalman009@gmail.com>
2022-04-11 09:26:56 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char c, C;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (C ='A'; C <= 'Z'; C++)
+		putchar(C);
+	putchar('\n');
+
+	return (0);
+}
+

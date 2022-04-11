e3b5804cb2f182999ec21015826598836f73bdb6
itsmmagain <mmtsalman009@gmail.com>
2022-04-11 15:50:05 -0700

    mm

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - entry point
+ *main - Entry point
+ *Return: Always 1
  *
- * Return always 0 (success)
  */
-void main(void)
+int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar,");
-	fprintf(stderr, "2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-

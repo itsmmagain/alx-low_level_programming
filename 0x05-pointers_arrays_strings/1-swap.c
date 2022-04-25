1cd65480981a43d032475bf15026300296f4e79c
itsmmagain <mmtsalman009@gmail.com>
2022-04-25 07:12:56 -0700

    1-swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,15 @@
 #include"main.h"
 /**
- * swap_int: function to swap integer value
- * a is first integer
- * b is second integer
+ * swap_int - function to swap integer value
+ * @a: is first integer
+ * @b: is second integer
  *
  */
-void swap_int(int *a, int *b) 
+void swap_int(int *a, int *b)
 {
 	int i;
-	i = *a; 
-	*a = *b; 
-	*b = i; 
+
+	i = *a;
+	*a = *b;
+	*b = i;
 }

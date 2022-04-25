107f1b8b99e5a37e28835fb5802bc658c3c858d5
itsmmagain <mmtsalman009@gmail.com>
2022-04-25 11:14:12 -0700

    0-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * reset_to_98 - update parameter value to 98
+ * @n: value to be updated
+ * Return: void
+ */
+void reset_to_98(int *n)
+
+{
+	int i;
+
+	i = 98;
+	*n = i;
+}

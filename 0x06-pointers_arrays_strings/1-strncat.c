df2bcc2b951009f514c959e6ff6c41c991045a47
itsmmagain <mmtsalman009@gmail.com>
2022-04-26 10:00:17 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,15 +5,18 @@
  * @dest: character string to concatenate to
  * @src: character string that is concatenated
  * Return: pointer to the resulting string dest
+ * @n: number of strings from the src to concatenate
  */
 char *_strncat(char *dest, char *src, int n)
 {
+
 	int i = 0, j = 0;
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
+	
 	while (src[j] != src[n])
 	{
 		dest[i] = src[j];

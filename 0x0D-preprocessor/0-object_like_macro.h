3219dc811af3d2a1b5f5b945c4bf56859e012c0e
itsmmagain <mmtsalman009@gmail.com>
2022-05-11 07:38:35 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,9 @@
+/**
+ * DEFINING A MACRO IN A HEADER FILE IN C
+ * SIZE: name of the macro 
+ * 1024 is the value of the macro
+ * Desc: whenever MAX_ARRAY_LENGTH is called, it will be replace with the value 20
+ */
+
+#define SIZE 1024
+
